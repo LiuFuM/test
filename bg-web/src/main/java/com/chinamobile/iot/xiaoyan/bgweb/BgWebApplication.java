23d2f265b376326628610a325afb340a7a6867a0
borlin <821074027@qq.com>
2020-05-12 20:41:44 +0800

    提交bg-web

---
+++ bg-web/src/main/java/com/chinamobile/iot/xiaoyan/bgweb/BgWebApplication.java
@@ -0,0 +1,23 @@
+package com.chinamobile.iot.xiaoyan.bgweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@EnableEurekaClient
+@RestController
+public class BgWebApplication {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "OK";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgWebApplication.class, args);
+    }
+}
+
